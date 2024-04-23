0e0a22ae05b620071994d5df4e136acedeb00444
Mikhail <entin@bastard.games>
2024-04-23 19:06:02 +0300

    fixes

--- Geometry-test/Example/Core/Shape/TriangleStruct.cs
+++ Geometry-test/Example/Core/Shape/TriangleStruct.cs
@@ -22,4 +22,10 @@ public readonly struct TriangleStruct : ITriangle
     public double GetArea() => Triangle.GetAreaByThreeSides(A_Length, B_Length, C_Length);
 
     public bool IsRightAngled() => Triangle.IsRightAngledByThreeSides(A_Length, B_Length, C_Length);
+    
+    public bool Equals(TriangleStruct other) => A_Length.Equals(other.A_Length) && B_Length.Equals(other.B_Length) && C_Length.Equals(other.C_Length);
+
+    public override bool Equals(object? obj) => obj is TriangleStruct other && Equals(other);
+
+    public override int GetHashCode() => HashCode.Combine(A_Length, B_Length, C_Length);
 }
